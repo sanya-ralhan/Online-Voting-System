841b43377baefbf0b8054fe92851ce0b4efd46e6
sanya ralhan <sanyaralhan@sanyas-MacBook-Air.local>
2016-12-08 14:02:23 -0500

    web.config

--- OnlineVotingSystem/Models/SystemDbNaya.cs
+++ OnlineVotingSystem/Models/SystemDbNaya.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-//using System.Data.Entity;
-
-
-namespace OnlineVotingSystem.Models
+﻿namespace OnlineVotingSystem.Models
 {
-	public class SystemDbNaya : DbContext
-    {
-		public System.Data.Entity.DbSet<User> Users{get;set;}
-        public DbSet<Voter> Voters{get;set;}
-        public DbSet<Candidate> Candidates{get; set;}
+	public class SystemDbNaya : System.Data.Entity.DbContext
+	{
+		public System.Data.Entity.DbSet<User> Users { get; set; }
+		public System.Data.Entity.DbSet<Voter> Voters { get; set; }
+		public System.Data.Entity.DbSet<Candidate> Candidates { get; set; }
 
-    }
+	}
 }
